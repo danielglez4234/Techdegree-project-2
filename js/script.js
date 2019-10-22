cc5c329c982e1878b4be0571b79960c521f48d4c
danielglez4234 <gnzlzdaniel.task@gmail.com>
2019-10-22 18:08:10 +0100

    meets-expectarion - ready

--- js/script.js
+++ js/script.js
@@ -22,8 +22,42 @@ const itemsPerPage = {
   showPrevNext: false,
   hidePageNumbers: false
 }
+/***
+   Create the `showPage` function to hide all of the items in the
+   list except for the ten you want to show.
+
+   Pro Tips:
+     - Keep in mind that with a list of 54 students, the last page
+       will only display four.
+     - Remember that the first student has an index of 0.
+     - Remember that a function `parameter` goes in the parens when
+       you initially define the function, and it acts as a variable
+       or a placeholder to represent the actual function `argument`
+       that will be passed into the parens later when you call or
+       "invoke" the function
+***/
+
+const showpage = (list, page) => {
+
+const startIndex = (page * itemsPerPage.perPage) - itemsPerPage.perPage;
+const endIndex = page * itemsPerPage.perPage - 1;
+
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i <= endIndex) {
+      list[i].style.display = '';
+    }else {
+      list[i].style.display = 'none';
+    }
+  }
+}
+
+showpage(list, 1);
 
 
+/***
+   Create the `appendPageLinks function` to generate, append, and add
+   functionality to the pagination buttons.
+***/
 
 
 const appendPageLinks = (list) => {
@@ -41,66 +75,27 @@ const appendPageLinks = (list) => {
   ul.appendChild(li);
 
 
-      const pageTotal = list.length / itemsPerPage.perPage;
-          function append_a(i){
-            const num = 1;
-            const a = document.createElement('a');
-            a.textContent = num + i;
-            a.href = '#' + (num + i);
-            a.id = num + i;
-            li.appendChild(a);
-          }
-     for (let i = 0; i < pageTotal; i++) {
-       append_a(i);
-     }
+    const pageTotal = list.length / itemsPerPage.perPage;
+
+    for (let i = 0; i < pageTotal; i++) {
+      const num = 1;
+      const a = document.createElement('a');
+      a.textContent = num + i;
+
+      a.addEventListener('click', (event) =>{
+        showpage(list, a.textContent);
+        const activeA = document.querySelectorAll('li a');
+            for (var i = 0; i < activeA.length; i++) {
+              activeA[i].className = '';
+            }
+        event.target.className = 'active';
+      });
+
+    li.appendChild(a);
+    }
+    // This 'const' gets the first 'a' of the 'li' and adds the active class - this is because when we enters the page we will allways be on page 1
+    const firstActive = document.querySelector('li a');
+    firstActive.className = 'active';
 
 }
 appendPageLinks(list);
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-
-const showpage = (list, page) => {
-
-const startIndex = (page * itemsPerPage.perPage) - itemsPerPage.perPage;
-const endIndex = page * itemsPerPage.perPage - 1;
-
-  for (let y = 0; y < list.length; y++) {
-    if (y >= startIndex && y <= endIndex) {
-      list[y].style.display = '';
-    }else {
-      list[y].style.display = 'none';
-    }
-  }
-}
-
-showpage(list, 1);
-
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
